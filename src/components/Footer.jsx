4c1e2d0d8bd3f09cb4c9488a0c169838a71262f1
Soko77788 <NickSokolowski7@gmail.com>
2024-07-29 07:38:27 -0500

    components for footer, header, nav, projects added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Connect with me:</p>
+      <a href="https://github.com/Soko77788">GitHub</a>
+      <a href="https://www.linkedin.com/in/your-nick-sokolowski">LinkedIn</a>
+      <a href="https://x.com/nick-sokolowski">Stack Overflow</a>
+    </footer>
+  );
+};
+
+export default Footer;
