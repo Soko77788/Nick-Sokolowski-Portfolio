68f9c62e449506da3434dc5a32c780b6a4bf537e
Soko77788 <NickSokolowski7@gmail.com>
2024-07-31 15:57:44 -0500

    footer styled, Aboutme page photo/styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Connect with me:</p>
-      <a href="https://github.com/Soko77788">GitHub</a>
-      <a href="https://www.linkedin.com/in/your-nick-sokolowski">LinkedIn</a>
-      <a href="https://x.com/nick-sokolowski">Stack Overflow</a>
+    <footer className="footer">
+      <p>Connect with me</p>
+      <div className="footer-links">
+        <a href="https://github.com/Soko77788">GitHub</a>
+        <a href="https://www.linkedin.com/in/your-nick-sokolowski">LinkedIn</a>
+        <a href="https://x.com/nick-sokolowski">Stack Overflow</a>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
+
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <section>
+    <section className="about-me">
       <h2>About Me</h2>
-      <img src="/path/to/your/photo.jpg" alt="Developer" />
+      <img src="./src/assets/selfie.jpg" alt="Developer" className="about-me-photo" />
       <p>
         My name is Nick Sokolowski and I'm a full stack developer from Wisconsin. I'm 35 years old and married. My interests include fitness, sports, and
         nerd culture such as gaming and anime. As a developer I'm hoping to work on an application in one of these spaces one day! Looking to settle into a career that is constantly evolving and I can master as I
