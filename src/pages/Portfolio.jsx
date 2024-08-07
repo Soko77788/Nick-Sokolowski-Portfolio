cc2effdc478b4dc795fc936ad4c2bfc06b23f2bd
Soko77788 <NickSokolowski7@gmail.com>
2024-08-07 16:59:17 -0500

    project images added and styling projects

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,38 +3,38 @@ import Project from '../components/Project';
 
 const projects = [
   {
-    title: 'Holidb Full Stack Web',
+    title: 'Holidb Full Stack App',
     image: './src/assets/Holidb.png',
     deployedLink: 'https://holidb.onrender.com/',
     repoLink: 'https://github.com/your-github/project1'
   },
   {
     title: 'Radlibs Front-end',
-    image: '/path/to/project1.png',
+    image: './src/assets/radlibs.png',
     deployedLink: 'https://mmhilbert.github.io/madlibs-project/',
     repoLink: 'https://github.com/your-github/project1'
   },
   {
     title: 'Weather App Front-end',
-    image: '/path/to/project1.png',
+    image: './src/assets/weather-app.png',
     deployedLink: 'https://soko77788.github.io/weather-dashboard-hw-6/',
     repoLink: 'https://github.com/your-github/project1'
   },
   {
     title: 'Express Note Taker',
-    image: '/path/to/project1.png',
+    image: './src/assets/note-taker.png',
     deployedLink: 'https://hw-11-express-note-taker.onrender.com/',
     repoLink: 'https://github.com/your-github/project1'
   },
   {
     title: 'MVC Bloggin It!',
-    image: '/path/to/project1.png',
+    image: './src/assets/blogginit.png',
     deployedLink: 'https://hw-14-mvc-blog-site.onrender.com/',
     repoLink: 'https://github.com/your-github/project1'
   },
   {
     title: 'Back-end SVG Logo Generator',
-    image: '/path/to/project1.png',
+    image: './src/assets/svg-logo-gen.png',
     deployedLink: 'https://drive.google.com/file/d/1bZEXALI_VwKr0tdS8m--rI7wYhV9Mmkj/view?pli=1',
     repoLink: 'https://github.com/your-github/project1'
   },
